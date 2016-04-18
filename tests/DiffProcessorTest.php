15a1cf6068b2ac2a0503dadad97da255fdab146f
David Yang <daworld.ny@gmail.com>
2016-04-18 07:32:43 +0500

    Added test for new sentences

--- tests/DiffProcessorTest.php
+++ tests/DiffProcessorTest.php
@@ -4,4 +4,12 @@ use TextCvs\DiffProcessor;
 
 class DiffProcessorTest extends \PHPUnit_Framework_TestCase
 {
+    public function testCanAddNewSentence()
+    {
+        $old = 'The first.';
+        $new = 'The first. The new one.';
+
+        $processor = new DiffProcessor($old, $new);
+        $this->assertTrue($processor->isNew(' The new one.'));
+    }
 }
